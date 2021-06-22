0900f1182349fb786e38bd1cfb903e2cb49f2d0c
wendeldemelo <demelowendel@gmail.com>
2021-06-22 02:14:34 -0400

    requisitos obrigatorios finalizados

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ submitButton.addEventListener('click', () => {
     alert('Login ou senha inválidos.');
   }
 });
+
+function changeDisabled() {
+  const confirm = document.getElementById('agreement');
+  const sendBtn = document.getElementById('submit-btn');
+
+  if (confirm.checked) {
+    sendBtn.disabled = false;
+  } else {
+    sendBtn.disabled = true;
+  }
+}
+
+const confirmBtn = document.getElementById('agreement');
+confirmBtn.addEventListener('click', changeDisabled);
+
+// créditos: http://www.codigoti.com.br/2018/04/javascript-contando-caracteres-dentro.html
+
+const counterText = document.getElementById('counter');
+function counterBox(event) {
+  const { target } = event;
+  const longitudMax = event.target.getAttribute('maxlength');
+  const longitudAct = target.value.length;
+  counterText.innerHTML = `${longitudAct}/${longitudMax}`;
+}
+const boxText = document.getElementById('textarea');
+boxText.addEventListener('input', counterBox);
